34a5a85cb816a0d7b7b4b1e074f29a781ae0a8cb
miss-gif <ddc.akari@gmail.com>
2024-05-19 16:22:13 +0900

    Feat : 라우터 및 글로벌 css 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
+import DetailPage from "./pages/DetailPage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import OrderPage from "./pages/OrderPage";
+import RegisterPage from "./pages/RegisterPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="product/:id" element={<DetailPage />} />
+            <Route path="cart" element={<CartPage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="register" element={<RegisterPage />} />
+            <Route path="order" element={<OrderPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
