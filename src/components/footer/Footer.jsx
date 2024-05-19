603fb57c80d3574bffe7934562df2d7445d9f157
miss-gif <ddc.akari@gmail.com>
2024-05-19 17:22:47 +0900

    Design : footer 스타일 업데이트

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { BsGithub } from "react-icons/bs";
+import styles from "./Footer.module.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className="container">
+        <div className={styles.contacts}>
+          <a href="#">
+            <BsGithub />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
